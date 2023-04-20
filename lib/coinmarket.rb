b3fccad73a3e4a571b7ccec5eeb00c6e5239a678
theonapoly <theo.napoly@gmail.com>
2023-04-20 15:05:56 +0200

    fin exercice 1, crypto

--- lib/coinmarket.rb
+++ lib/coinmarket.rb
@@ -3,9 +3,16 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
 
-crypto_name_array = doc.xpath('//tbody/tr/td[5]').each do |name|
-puts name.text
+crypto_name_array = doc.xpath('//tbody/tr/td[3]').map do |name|
+    name.text
+end.take(20)
+
+crypto_price_array = doc.xpath('//tbody/tr/td[5]').map do |price|
+    price.text
+end.take(20)
+
+crypto_array = crypto_name_array.zip(crypto_price_array).map do |name, price| # Zip = combiner les deux tableaux
+    { name: name, price: price }.values.join(" ")
 end
 
-result_array = []
-
+puts crypto_array
